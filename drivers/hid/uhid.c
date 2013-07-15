bece40bdbdc6eae9c914c4b55614aaaa236ceca2
David Herrmann <dh.herrmann@gmail.com>
2013-07-15 19:10:12 +0200

    HID: input: generic hidinput_input_event handler
    The hidinput_input_event() callback converts input events written from
    userspace into HID reports and sends them to the device. We currently
    implement this in every HID transport driver, even though most of them do
    the same.
    
    This provides a generic hidinput_input_event() implementation which is
    mostly copied from usbhid. It uses a delayed worker to allow multiple LED
    events to be collected into a single output event.
    We use the custom ->request() transport driver callback to allow drivers
    to adjust the outgoing report and handle the request asynchronously. If no
    custom ->request() callback is available, we fall back to the generic raw
    output report handler (which is synchronous).
    
    Drivers can still provide custom hidinput_input_event() handlers (see
    logitech-dj) if the generic implementation doesn't fit their needs.
    
    Conflicts:
    	drivers/hid/hid-input.c
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Michael Wright <michaelwr@google.com>
    Change-Id: I89ccc3f675b3e8a7bbd812a6ebe12588d737cfd6
    
    Conflicts:
    	drivers/hid/uhid.c

--- drivers/hid/uhid.c
+++ drivers/hid/uhid.c
@@ -115,55 +115,6 @@ static void uhid_hid_close(struct hid_device *hid)
 	uhid_queue_event(uhid, UHID_CLOSE);
 }
 
-static int uhid_hid_input(struct input_dev *input, unsigned int type,
-			  unsigned int code, int value)
-{
-	struct hid_device *hid = input_get_drvdata(input);
-	struct uhid_device *uhid = hid->driver_data;
-	unsigned long flags;
-	struct uhid_event *ev;
-	struct hid_field *field;
-	struct hid_report *report;
-	int offset;
-
-	ev = kzalloc(sizeof(*ev), GFP_ATOMIC);
-	if (!ev)
-		return -ENOMEM;
-
-	switch (type) {
-	case EV_LED:
-		offset = hidinput_find_field(hid, type, code, &field);
-		if (offset == -1) {
-			hid_warn(input, "event field not found\n");
-			kfree(ev);
-			return -1;
-		}
-
-		hid_set_field(field, offset, value);
-
-		report = field->report;
-
-		ev->type = UHID_OUTPUT;
-		ev->u.output.rtype = UHID_OUTPUT_REPORT;
-		hid_output_report(report, ev->u.output.data);
-		ev->u.output.size = ((report->size - 1) >> 3) + 1 +
-							(report->id > 0);
-		break;
-
-	default:
-	ev->type = UHID_OUTPUT_EV;
-	ev->u.output_ev.type = type;
-	ev->u.output_ev.code = code;
-	ev->u.output_ev.value = value;
-	}
-
-	spin_lock_irqsave(&uhid->qlock, flags);
-	uhid_queue(uhid, ev);
-	spin_unlock_irqrestore(&uhid->qlock, flags);
-
-	return 0;
-}
-
 static int uhid_hid_parse(struct hid_device *hid)
 {
 	struct uhid_device *uhid = hid->driver_data;
@@ -297,7 +248,6 @@ static struct hid_ll_driver uhid_hid_driver = {
 	.stop = uhid_hid_stop,
 	.open = uhid_hid_open,
 	.close = uhid_hid_close,
-	.hidinput_input_event = uhid_hid_input,
 	.parse = uhid_hid_parse,
 };
 
