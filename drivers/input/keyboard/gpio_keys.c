d903b895e990b96704932c7563d3214c5a124ea7
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-18 07:47:09 -0400

    picassowifi: SW_LID needs the inverted state from SW_FLIP

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -892,7 +892,7 @@ static void flip_cover_work(struct work_struct *work)
 	if(first == second && ddata->flip_cover != first) {
 		ddata->flip_cover = first;
 		input_report_switch(ddata->input,
-			SW_LID, ddata->flip_cover);
+			SW_LID, !ddata->flip_cover);
 		input_sync(ddata->input);
 	}
 }
@@ -912,7 +912,7 @@ static void flip_cover_work(struct work_struct *work)
 	if(ddata->flip_cover != first) {
 		ddata->flip_cover = first;
 		input_report_switch(ddata->input,
-			SW_LID, ddata->flip_cover);
+			SW_LID, !ddata->flip_cover);
 		input_sync(ddata->input);
 	}
 }
