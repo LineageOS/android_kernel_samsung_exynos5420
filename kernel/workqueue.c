82f72829c2b54e1e7bd54008ce701057e4f03492
Christopher R. Palmer <crpalmer@gmail.com>
2014-10-23 05:23:17 -0400

    fix bad merge of kernel/workqueue.c
    Change-Id: I92c1529562aadf9d517193c8b738b3dd2e65eb7b

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1900,14 +1900,11 @@ __acquires(&gcwq->lock)
 	lock_map_acquire(&lockdep_map);
 	trace_workqueue_execute_start(work);
 
-	sec_debug_work_log(worker, work, f, 1);
-
 	worker->current_func(work);
 	/*
 	 * While we must be careful to not use "work" after this, the trace
 	 * point will only record its address.
 	 */
-	sec_debug_work_log(worker, work, f, 2);
 	trace_workqueue_execute_end(work);
 	lock_map_release(&lockdep_map);
 	lock_map_release(&cwq->wq->lockdep_map);
